7070afd87ea6ca0c5a25c319b11191d36ab9fc8f
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-10-24 22:13:58 +0700

    clean

--- Dynamics.Models/Models/ViewModel/UserHistoryViewModel.cs
+++
@@ -1,8 +0,0 @@
-using Dynamics.Models.Models.DTO;
-
-namespace Dynamics.Models.Models.ViewModel;
-
-public class UserHistoryViewModel
-{
-    public List<UserTransactionDto> userTransactions { get; set; }
-}

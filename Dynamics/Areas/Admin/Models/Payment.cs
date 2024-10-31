99072842210e774f1031e918c08ed1a0072cfdff
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-10-31 16:12:05 +0700

    Update model

--- Dynamics/Areas/Admin/Models/Payment.cs
+++ Dynamics/Areas/Admin/Models/Payment.cs
@@ -4,8 +4,12 @@ namespace Dynamics.Areas.Admin.Models
 {
     public class Payment
     {
+        public dynamic listTransaction { get; set; }
         public List<UserToProjectTransactionHistory> listUserToProject { get; set; }
         public List<OrganizationToProjectHistory> listOrganizationToProject { get; set; }
         public List<UserToOrganizationTransactionHistory> listUserToOrganization { get; set; }
+        public List<ProjectResource> listUserDonateToProjectTable { get; set; }
+        public List<OrganizationResource> listUserDonateToOrganizationTable { get; set; }
+        public List<OrganizationResource> listOrganizationDonateToProjectTable { get; set; }
     }
 }

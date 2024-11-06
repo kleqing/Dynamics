e4e967d76b6677af5ab405f7f5cfdef44e736b4a
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-11-06 18:11:54 +0700

    Update payment model (mvvm)

--- Dynamics/Areas/Admin/Models/Payment.cs
+++ Dynamics/Areas/Admin/Models/Payment.cs
@@ -6,10 +6,7 @@ namespace Dynamics.Areas.Admin.Models
     {
         public dynamic listTransaction { get; set; }
         public List<UserToProjectTransactionHistory> listUserToProject { get; set; }
-        public List<OrganizationToProjectHistory> listOrganizationToProject { get; set; }
-        public List<UserToOrganizationTransactionHistory> listUserToOrganization { get; set; }
         public List<ProjectResource> listUserDonateToProjectTable { get; set; }
-        public List<OrganizationResource> listUserDonateToOrganizationTable { get; set; }
-        public List<OrganizationResource> listOrganizationDonateToProjectTable { get; set; }
+        public List<Withdraw> listWithdraws { get; set; }
     }
 }

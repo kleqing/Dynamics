2b0399c94ffb01b7476909516dc34dffc5878c72
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-11-07 00:21:28 +0700

    Change dynamic to List

--- Dynamics/Areas/Admin/Models/Payment.cs
+++ Dynamics/Areas/Admin/Models/Payment.cs
@@ -4,7 +4,7 @@ namespace Dynamics.Areas.Admin.Models
 {
     public class Payment
     {
-        public dynamic listTransaction { get; set; }
+        public List<TransactionBase> listTransaction { get; set; }
         public List<UserToProjectTransactionHistory> listUserToProject { get; set; }
         public List<ProjectResource> listUserDonateToProjectTable { get; set; }
         public List<Withdraw> listWithdraws { get; set; }

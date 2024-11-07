90ad092bc4ca72b0ae496ae07d611e5aa531bcb9
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-11-07 12:30:00 +0700

    rename properties

--- Dynamics/Areas/Admin/Models/Payment.cs
+++ Dynamics/Areas/Admin/Models/Payment.cs
@@ -4,7 +4,7 @@ namespace Dynamics.Areas.Admin.Models
 {
     public class Payment
     {
-        public List<TransactionBase> listTransaction { get; set; }
+        public List<TransactionBase> viewwithdraw { get; set; }
         public List<UserToProjectTransactionHistory> listUserToProject { get; set; }
         public List<ProjectResource> listUserDonateToProjectTable { get; set; }
         public List<Withdraw> listWithdraws { get; set; }

bc0b08d1486ce0646c7f0e108f05557d668ce7ea
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-11-09 00:35:58 +0700

    add db entry

--- Dynamics.DataAccess/Repository/UserRepository.cs
+++ Dynamics.DataAccess/Repository/UserRepository.cs
@@ -75,6 +75,7 @@ namespace Dynamics.DataAccess.Repository
         //
         public async Task<bool> UpdateAsync(User user)
         {
+            _db.Entry(user).State = EntityState.Detached;
             var existingItem = await GetAsync(u => user.Id == u.Id);
             if (existingItem == null)
             {

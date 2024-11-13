8ea1e89aa71403cc23c273573ebcad32d2e2f454
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-11-13 11:56:23 +0700

    update db

--- Dynamics.DataAccess/Migrations/20241113045611_Initial.Designer.cs
+++ Dynamics.DataAccess/Migrations/20241113045611_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dynamics.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241109034451_Initial")]
+    [Migration("20241113045611_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
